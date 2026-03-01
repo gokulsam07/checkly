e54a2d766cda77d9da0ec6e560138b19d0d3ea86
Gokul Saminathan <gsaminathan@opentext.com>
2026-03-01 16:12:31 +0530

    fix: updated selenide.yml & added data purging before starting tests

--- src/test/java/api/EnvVarAPITest.java
+++ src/test/java/api/EnvVarAPITest.java
@@ -2,9 +2,10 @@ package api;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import org.json.JSONArray;
-import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,7 +14,24 @@ import rest.core.JSONIterable;
 import rest.core.RestController;
 
 public class EnvVarAPITest {
-	private RestController api = null;
+	private RestController api = new RestController();
+	
+	@BeforeClass
+	public void prugeData() throws IOException {
+		Response res = api.get("variables", null);
+		List<Map<String, String>> variables = res.jsonPath().getList("$");
+		if (variables != null && !variables.isEmpty()) {
+		    variables.stream()
+		        .map(var -> var.get("key"))        
+		        .filter(key -> key != null)        
+		        .forEach(key -> {
+		            System.out.println("Deleting key: " + key);
+		            api.delete("variables/{varId}", Map.of("varId", key)).then().statusCode(204);
+		        });
+		} else {
+		    System.out.println("No variables found. Skipping deletion.");
+		}
+	}
 
 	@BeforeMethod
 	public void setup() {
